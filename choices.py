ab31463ce69741dc64460ab55a3bdd23b381c106
Richard Smith-Unna <richardsmith404@gmail.com>
2016-03-09 16:19:03 +0000

    Save scripts from software carpentry

---
+++ choices.py
@@ -0,0 +1,13 @@
+num = -110
+
+if (num < 0) or (num > 0):
+    print(num, 'is not zero')
+
+if num > 0:
+    print(num, 'is positive')
+elif num == 0:
+    print(num, 'is zero')
+else:
+    print(num, 'is negative')
+
+print('done')
